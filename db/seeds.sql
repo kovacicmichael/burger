3f3ac63e1045eda8bcf62299c6c0df0cddd4d640
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-03-02 16:08:12 -0700

    new files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Texas Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
